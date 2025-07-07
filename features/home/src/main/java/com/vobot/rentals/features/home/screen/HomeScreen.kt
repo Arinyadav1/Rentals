bbae8912d0ecc9c32d7e651f774104789a4a09f7
Arin Yadav <arinyadav98@gmail.com>
2025-07-07 22:06:15 +0530

    add gender category

--- features/home/src/main/java/com/vobot/rentals/features/home/screen/HomeScreen.kt
+++ features/home/src/main/java/com/vobot/rentals/features/home/screen/HomeScreen.kt
@@ -3,10 +3,14 @@ package com.vobot.rentals.features.home.screen
 
 import android.R.attr.onClick
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,6 +25,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
@@ -50,6 +55,8 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -68,6 +75,8 @@ import androidx.compose.ui.util.lerp
 import coil.compose.AsyncImage
 import com.vobot.rentals.features.home.R
 import com.vobot.rentals.features.search.screen.SearchScreen
+import kotlinx.coroutines.delay
+import kotlin.collections.listOf
 import kotlin.math.absoluteValue
 
 
@@ -109,8 +118,7 @@ fun HomeScreen(
 
                                 IconButton(onClick = {}) {
                                     Icon(
-                                        Icons.Default.Menu,
-                                        contentDescription = "menu"
+                                        Icons.Default.Menu, contentDescription = "menu"
                                     )
                                 }
 
@@ -137,16 +145,14 @@ fun HomeScreen(
                         modifier
                             .clickable(
                                 indication = null,
-                                interactionSource = remember { MutableInteractionSource() }
-                            ) {}
+                                interactionSource = remember { MutableInteractionSource() }) {}
                             .fillMaxWidth(.87f)
                             .height(48.dp)
                             .padding(bottom = 10.dp)
                             .border(.0.dp, Color.Gray, CircleShape),
                         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.onPrimary),
                         shape = CircleShape,
-                        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 5.dp)
-                    ) {
+                        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 5.dp)) {
                         Box(
                             modifier
                                 .fillMaxSize()
@@ -169,8 +175,7 @@ fun HomeScreen(
                                         .padding(start = 20.dp),
                                 ) {
                                     Text(
-                                        text = "Search",
-                                        style = MaterialTheme.typography.bodyLarge
+                                        text = "Search", style = MaterialTheme.typography.bodyLarge
                                     )
                                 }
                             }
@@ -285,13 +290,21 @@ fun HomeScreen(
         Box(
             modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.TopCenter
+                .padding(innerPadding), contentAlignment = Alignment.TopCenter
         ) {
+            LazyColumn(
+                flingBehavior = ScrollableDefaults.flingBehavior()
+            ) {
+                item {
+                    Column(
+                        modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        CarouselCard(modifier)
 
-            LazyColumn {
-                items(1) {
-                    CarouselCard()
+                        GenderRow(modifier)
+                    }
                 }
             }
 
@@ -301,77 +314,163 @@ fun HomeScreen(
 
 
 @Composable
-fun CarouselCard(modifier: Modifier = Modifier) {
+private fun CarouselCard(modifier: Modifier) {
 
-   val count = 5
+    val images = listOf(
+        "https://fastly.picsum.photos/id/6/1000/500.jpg?hmac=6f7s_24z1XgQ6QElGrW4fwsRKLIfmuWowrecEh86nbo",
+        "https://fastly.picsum.photos/id/858/1000/500.jpg?hmac=FZDw6ZCjfr0ZvTzR8qt_mPh6ojXHYqkUeDqfIjFtApU",
+        "https://fastly.picsum.photos/id/731/1000/500.jpg?hmac=aGhjPlHv_FuXmnDEuX-bHarVupjAZmVwwI1Bi5g7GFw",
+        "https://fastly.picsum.photos/id/700/1000/500.jpg?hmac=VBA1B3YbuCIYGXpWIlqLA6tZHHIl6qQWx6Y0wm3L_tw",
+        "https://fastly.picsum.photos/id/819/1000/500.jpg?hmac=NFbEoH6UbZ3MprU6NT-kqg_DAVw4Co3lp4cxdkXehzE"
+    )
 
-    val pagerState = rememberPagerState(initialPage = count/2, pageCount = { count })
+    val max = Int.MAX_VALUE
+    val pagerState = rememberPagerState(initialPage = max / 2, pageCount = { max })
 
-    Column(
-    ) {
-        HorizontalPager(
-            state = pagerState,
-            contentPadding = PaddingValues(horizontal = 30.dp),
+    val pagerIsDragged by pagerState.interactionSource.collectIsDraggedAsState()
 
-            ) { page ->
-            Card(
+    val pageInteractionSource = remember { MutableInteractionSource() }
+    val pageIsPressed by pageInteractionSource.collectIsPressedAsState()
 
-                shape = RoundedCornerShape(16.dp),
-                elevation = CardDefaults.cardElevation(pressedElevation = 10.dp, defaultElevation = 8.dp),
+    val autoAdvance = !pagerIsDragged && !pageIsPressed
 
-                modifier = modifier
-                    .fillMaxWidth(1f)
-                    .padding(horizontal = 10.dp)
-                    .height(200.dp)
-
-                    .graphicsLayer {
-                        val pageOffset = (
-                                (pagerState.currentPage - page) + pagerState
-                                    .currentPageOffsetFraction
-                                ).absoluteValue
-
-                        scaleY = lerp(
-                            start = 0.85f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, .1f)
-                        )
-                        scaleX = lerp(
-                            start = 0.85f,
-                            stop = 1f,
-                            fraction = 1f - pageOffset.coerceIn(0f, .1f)
-                        )
-                    },
-            ) {
-                AsyncImage(
-                    model = "https://fastly.picsum.photos/id/726/1000/400.jpg?hmac=g1e6bkiB6m15QapO6e2MI258HSrVUoymk95nZ5eR8MQ",
-                    contentDescription = "image",
-                    contentScale = ContentScale.FillBounds,
-                    modifier = Modifier.fillMaxSize()
-                )
+    if (autoAdvance) {
+        LaunchedEffect(pagerState, pageInteractionSource) {
+            while (true) {
+                delay(3000)
+                val nextPage = (pagerState.currentPage + 1)
+                pagerState.animateScrollToPage(nextPage)
             }
         }
-
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp, top = 5.dp),
-            horizontalArrangement = Arrangement.Center,
-        ) {
-            repeat(pagerState.pageCount) { iteration ->
-                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
-                Box(
-                    modifier = Modifier
-                        .padding(2.dp)
-                        .clip(CircleShape)
-                        .background(color)
-                        .size(8.dp)
-                )
-            }
-        }
-
-
     }
 
+
+    HorizontalPager(
+        state = pagerState,
+        pageSpacing = (-35).dp,
+        contentPadding = PaddingValues(horizontal = 30.dp),
+
+        ) { page ->
+
+        ElevatedCard(
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(
+                pressedElevation = 10.dp, defaultElevation = 8.dp
+            ),
+
+            modifier = modifier
+                .fillMaxWidth(1f)
+                .padding(horizontal = 10.dp)
+                .height(180.dp)
+                .clickable(
+                    interactionSource = pageInteractionSource, indication = LocalIndication.current
+                ) {
+
+                }
+
+                .graphicsLayer {
+                    val pageOffset =
+                        ((pagerState.currentPage - page) + pagerState.currentPageOffsetFraction).absoluteValue
+
+                    scaleY = lerp(
+                        start = 0.85f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                    scaleX = lerp(
+                        start = 0.85f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                    alpha = lerp(
+                        start = 0.4f, stop = 1f, fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                },
+        ) {
+            AsyncImage(
+                model = images[page % images.size],
+                contentDescription = "image",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp, top = 5.dp),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        repeat(images.size) { iteration ->
+            val color =
+                if ((pagerState.currentPage % images.size) == iteration) Color.DarkGray else Color.LightGray
+            Box(
+                modifier = Modifier
+                    .padding(2.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(7.dp)
+            )
+        }
+    }
+}
+
+
+@Composable
+fun GenderRow(modifier: Modifier) {
+
+    val genders = remember {
+        listOf(
+            R.string.man,
+            R.string.woman,
+            R.string.boy,
+            R.string.girl,
+        )
+    }
+
+
+    Column(
+        modifier.fillMaxWidth(.95f),
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier.fillMaxWidth(.9f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(R.string.gender_category),
+                style = MaterialTheme.typography.labelLarge
+            )
+            Text(
+                text = stringResource(R.string.see_all),
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+
+        LazyRow(
+            modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            items(genders.size) { it ->
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    ElevatedCard(
+                        modifier
+                            .padding(5.dp)
+                            .size(height = 160.dp, width = 130.dp)
+                    ) {
+
+                    }
+
+                    Text(
+                        text = stringResource(genders[it]),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
 }
 
 
